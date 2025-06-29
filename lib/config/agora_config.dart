4142e7262485a1bdb07f5d33f4ec7f67127c1e06
Fringe sunil <fringeks011@gmail.com>
2025-06-29 18:43:53 +0530

    Token Changed

--- lib/config/agora_config.dart
+++ lib/config/agora_config.dart
@@ -1,5 +1,5 @@
 class AgoraConfig {
   static const String appId = 'afd4f51259a04dee8b77fbb3cc10bbae';
   static const String token =
-      '007eJxTYBAQj/o50+OExglPu7TYnWf3usot3Oe3xt3l0r8Np88zWykrMCSmpZikmRoamVomGpikpKZaJJmbpyUlGScnGxokJSWmLt0el9EQyMiw3GsDMyMDBIL4bAzFRampxRkMDACtyyF0';
+      '007eJxTYEhtO3P3Sn3RgV+zFK+755btYHvYM/cDq1m1QNS68LZdL5coMCSmpZikmRoamVomGpikpKZaJJmbpyUlGScnGxokJSWm3rROzGgIZGSYxFLPwsgAgSA+G0NxUWpqcQYDAwBb+CK8';
 }
